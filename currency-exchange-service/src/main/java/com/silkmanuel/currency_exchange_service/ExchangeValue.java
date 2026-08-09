49cdcfdbab738d05c71f6f0c6dfe9c7fe1ee8d8e
Silkmanuel <107256054+Silkmanuel@users.noreply.github.com>
2026-08-09 20:29:36 +0100

    Create a Simple Hard-Coded Currency Exchange Service

---
+++ currency-exchange-service/src/main/java/com/silkmanuel/currency_exchange_service/ExchangeValue.java
@@ -0,0 +1,55 @@
+package com.silkmanuel.currency_exchange_service;
+
+import java.math.BigDecimal;
+
+/**
+ * ExchangeValue
+ */
+public class ExchangeValue {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    
+    public ExchangeValue() {
+    }
+
+    public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+        this.from = from;
+        this.to = to;
+        this.id = id;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+}
