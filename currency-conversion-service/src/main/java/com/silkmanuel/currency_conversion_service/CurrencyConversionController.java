92f5c3a4d994a1bb400c54db0b72dfe123849b5e
Silkmanuel <107256054+Silkmanuel@users.noreply.github.com>
2026-08-09 22:41:34 +0100

    Invoking Currency Exchange Microservice from Currency Conversion Micro

--- currency-conversion-service/src/main/java/com/silkmanuel/currency_conversion_service/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/silkmanuel/currency_conversion_service/CurrencyConversionController.java
@@ -1,12 +1,16 @@
 package com.silkmanuel.currency_conversion_service;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 public class CurrencyConversionController {
@@ -19,15 +23,24 @@ public class CurrencyConversionController {
 
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversationBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
-        CurrencyConversationBean currencyConversationBean = new CurrencyConversationBean(
-            10001L, 
-            from, 
-            to, 
-            BigDecimal.ONE, 
-            quantity, 
-            quantity 
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        ResponseEntity<CurrencyConversationBean> responseEntity = new RestTemplate().getForEntity(
+            "http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
+            CurrencyConversationBean.class, 
+            uriVariables
         );
-        currencyConversationBean.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        CurrencyConversationBean response = responseEntity.getBody();
+        CurrencyConversationBean currencyConversationBean = new CurrencyConversationBean(
+            response.getId(),
+            from,
+            to,
+            response.getConversionMultiple(),
+            quantity,
+            quantity.multiply(response.getConversionMultiple())
+        );
+        currencyConversationBean.setPort(response.getPort());
         return currencyConversationBean;
     }
 }
