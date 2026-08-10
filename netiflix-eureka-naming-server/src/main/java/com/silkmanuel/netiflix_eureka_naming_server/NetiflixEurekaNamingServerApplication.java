d17e57317b80a40b3321133df56b9d6458db78fb
Silkmanuel <107256054+Silkmanuel@users.noreply.github.com>
2026-08-10 01:22:31 +0100

    Understand the Need for a Naming Server

---
+++ netiflix-eureka-naming-server/src/main/java/com/silkmanuel/netiflix_eureka_naming_server/NetiflixEurekaNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.silkmanuel.netiflix_eureka_naming_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetiflixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetiflixEurekaNamingServerApplication.class, args);
+	}
+
+}
---
+++ netiflix-eureka-naming-server/src/test/java/com/silkmanuel/netiflix_eureka_naming_server/NetiflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silkmanuel.netiflix_eureka_naming_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetiflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
