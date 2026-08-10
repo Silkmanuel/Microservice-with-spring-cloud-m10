4e51ded22facf12a16cd1d385284a0df14a8fb10
Silkmanuel <107256054+Silkmanuel@users.noreply.github.com>
2026-08-10 01:30:12 +0100

    Setting up Eureka Naming Server

--- netiflix-eureka-naming-server/src/main/java/com/silkmanuel/netiflix_eureka_naming_server/NetiflixEurekaNamingServerApplication.java
+++ netiflix-eureka-naming-server/src/main/java/com/silkmanuel/netiflix_eureka_naming_server/NetiflixEurekaNamingServerApplication.java
@@ -2,12 +2,21 @@ package com.silkmanuel.netiflix_eureka_naming_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NetiflixEurekaNamingServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NetiflixEurekaNamingServerApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(NetiflixEurekaNamingServerApplication.class, args);
+		Environment environment = context.getBean(Environment.class);
+		System.out.println(
+			"INIT EUREKA NAMING SERVER - PORT: "
+			+ environment.getProperty("local.server.port")
+		);
 	}
 
 }
